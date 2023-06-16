71ceb91838925a451b8d6b696f98e0f0abd0fac3
Fred Oliveira <fred@helloform.com>
2023-06-16 12:04:24 +0100

    Bring in mitchellh's vmware tools module

--- machines/vm-pitch.nix
+++ machines/vm-pitch.nix
@@ -2,9 +2,20 @@
 
 {
   imports = [
+    ../modules/vmware-guest.nix
     ./vm-shared.nix
     ../hardware/vm-aarch64-fusion.nix
   ];
+
+  # Disable the original vmware module and use mitchellh's override
+  disabledModules = [ "virtualisation/vmware-guest.nix" ];
+
+  # Enable vmware guest support
+  virtualisation.vmware.guest = {
+    enable = true;
+    # TODO: For some reason, installation fails with headless = true. Need to figure out why
+    # headless = true;
+  };
 
   # Networking setup
   networking.hostName = "pitch";
@@ -31,5 +42,19 @@
       enable = true;
       port = 6379;
     };
+  };
+
+  # Share our host filesystem
+  fileSystems."/host" = {
+    fsType = "fuse./run/current-system/sw/bin/vmhgfs-fuse";
+    device = ".host:/";
+    options = [
+      "umask=22"
+      "uid=1000"
+      "gid=1000"
+      "allow_other"
+      "auto_unmount"
+      "defaults"
+    ];
   };
 }
